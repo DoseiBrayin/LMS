2d77c887d2dc3a9c9539ab751eb1717a8bd9781b
DoseiBrayin <ardilabrayan68@gmail.com>
2022-10-26 21:21:29 -0500

    (Config) Configuration nodejs server

--- src/index.js
+++ src/index.js
@@ -1,21 +1,40 @@
 const express = require("express");
-const morgan = require('morgan')
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const path = require('path');
 
 //Inicializar
 const app = express();
 
 //COnfiguracion
 app.set('port',process.env.PORT || 4000)
+app.set('views', path.join(__dirname,'views'))
+app.engine('.hbs', handlebars.engine({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'),'Layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+    helpers: require('./lib/handlebars.js')
+}));
+app.set('view engine', '.hbs')
 
 //Middleware
 app.use(morgan('dev'))
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 //Variables globales
+app.use((req,res,next)=>{
+    next();
+});
 
 //Rutas
+app.use(require('./routes'));
+app.use(require('./routes/auth'));
+app.use('/links',require('./routes/links'));
 
 //Publico
-
+app.use(express.static(path.join(__dirname,'public')));
 
 
 //Starting the server
