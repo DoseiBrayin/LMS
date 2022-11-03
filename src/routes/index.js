d08a0f9841ab54758a96e318071f46bcbb02c83f
DoseiBrayin <ardilabrayan68@gmail.com>
2022-11-03 09:20:09 -0500

    (Add) Adding views whit the server

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,11 @@ const router = express.Router()
 const pool = require('../database')
 
 router.get('/', (req, res) => {
-    res.render("layouts/Index/loginvista.hbs")
+    res.render("index")
+})
+
+router.get('/login', (req, res) => {
+    res.render("layouts/Index/loginvista")
 })
 
 module.exports = router
