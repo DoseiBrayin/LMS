55641d5f9691ca93946287f406aa217a2c953a8d
RubenArenasSaldarriaga89 <rdarenass@ucn.edu.co>
2022-11-03 21:43:09 -0500

    actualizacion vistas

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,11 @@ const router = express.Router()
 const pool = require('../database')
 
 router.get('/', (req, res) => {
-    res.render("index2")
+    res.render("indexest")
+})
+
+router.get('/cursos', (req, res) => {
+    res.render("cursosEst")
 })
 
 router.get('/login', (req, res) => {
