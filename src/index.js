986287db01480158b140b0e146a560e666418957
DoseiBrayin <ardilabrayan68@gmail.com>
2022-11-24 09:33:55 -0500

    (error) se soluciona el error en consultas de cursos de los usuarios estudiantes

--- src/index.js
+++ src/index.js
@@ -40,6 +40,7 @@ app.use(flash());
 app.use((req,res,next)=>{
     app.locals.success = req.flash('success')
     app.locals.message = req.flash('message')
+    app.locals.user = req.user
     next();
 });
 
--- src/routes/links.js
+++ src/routes/links.js
@@ -4,10 +4,10 @@ const router = express.Router()
 
 
 router.get('/cursos', async (req, res) => {
-    const sql = 'SELECT grupo_asignatura_id_asig,t.*,s.nombre as nombre_asig,p.nombre FROM (estudiante_has_grupo inner join (grupo g inner join tiempo t on g.tiempo_idtiempo=t.idtiempo) on estudiante_has_grupo.grupo_idgrupo=g.idgrupo) inner join asignatura s on estudiante_has_grupo.grupo_asignatura_id_asig=s.id_asig inner join profesor p on estudiante_has_grupo.grupo_profesor_cedula=p.cedula where estudiante_has_grupo.estudiante_cedula = 1883950'
-    const cursos =await pool.query(sql)
-    const sql_pensum = 'select e.nombre,e.cedula,e.pensum_idpensum,p.nombre as pensum  from pensum p inner join estudiante e on p.idpensum=e.pensum_idpensum where e.cedula = 1883950'
-    const  pensum = await pool.query(sql_pensum)
+    const sql = 'SELECT grupo_asignatura_id_asig,t.*,s.nombre as nombre_asig,p.nombre FROM (estudiante_has_grupo inner join (grupo g inner join tiempo t on g.tiempo_idtiempo=t.idtiempo) on estudiante_has_grupo.grupo_idgrupo=g.idgrupo) inner join asignatura s on estudiante_has_grupo.grupo_asignatura_id_asig=s.id_asig inner join profesor p on estudiante_has_grupo.grupo_profesor_cedula=p.cedula where estudiante_has_grupo.estudiante_usuario_idusuario = ?'
+    const cursos =await pool.query(sql,req.user.idusuario)
+    const sql_pensum = 'select e.nombre,e.cedula,e.pensum_idpensum,p.nombre as pensum  from pensum p inner join estudiante e on p.idpensum=e.pensum_idpensum where e.usuario_idusuario = ?'
+    const  pensum = await pool.query(sql_pensum,req.user.idusuario)
     res.render("links/estudiante/cursosEst",{cursos,pensum})
 })
 
