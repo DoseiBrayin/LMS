a722d58c495fe842bba5a0ceadbd2cd6be91fdfe
DoseiBrayin <ardilabrayan68@gmail.com>
2022-11-24 21:41:06 -0500

    (caracteristica)Se añaden los roles, al igual que sus verificaciones y vistas

--- src/lib/authentication.js
+++ src/lib/authentication.js
@@ -6,4 +6,25 @@ module.exports = {
         return res.redirect('/login');
     }
 
+    ,isStudent(req,res,next){
+        switch(req.user.rol_idrol){
+            case 1: return res.redirect('/links/indexAdmin')
+            case 2: return res.redirect('/links/indexprofe')
+            case 3: return next()
+        }
+    }
+    ,isTeacher(req,res,next){
+        switch(req.user.rol_idrol){
+            case 1: return res.redirect('/links/indexAdmin')
+            case 2: return next()
+            case 3: return res.redirect('/links/indexest')
+        }
+    },isAdmin(req,res,next){
+        switch(req.user.rol_idrol){
+            case 1: return next()
+            case 2: return res.redirect('/links/indexprofe')
+            case 3: return res.redirect('/links/indexest')
+        }
+    }
+
 };
