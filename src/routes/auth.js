e35450ee275270837f8bdfae6e509000744df36c
DoseiBrayin <ardilabrayan68@gmail.com>
2022-11-24 21:30:07 -0500

    (arreglo) Se arregla el inicio de sesion y se implementa la validacion de estar logueado

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,9 +3,17 @@ const router = express.Router()
 const passport=require('passport')
 
 
+
 router.get('/login', (req, res) => {
     res.render("login/loginvista")
 })
+
+router.get('/logout',function(req, res, next) {
+    req.logout(function(err) {
+      if (err) { return next(err); }
+      res.redirect('/login');
+    });
+  });
 
 router.post('/login', (req, res,next) => {
     passport.authenticate('local.signin',{
--- src/routes/links.js
+++ src/routes/links.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const pool = require('../database')
 const router = express.Router()
+const {isLoggedIn} = require('../lib/authentication')
 
 
-router.get('/cursos', async (req, res) => {
+router.get('/cursos',isLoggedIn, async (req, res) => {
     const sql = 'SELECT grupo_asignatura_id_asig,t.*,s.nombre as nombre_asig,p.nombre FROM (estudiante_has_grupo inner join (grupo g inner join tiempo t on g.tiempo_idtiempo=t.idtiempo) on estudiante_has_grupo.grupo_idgrupo=g.idgrupo) inner join asignatura s on estudiante_has_grupo.grupo_asignatura_id_asig=s.id_asig inner join profesor p on estudiante_has_grupo.grupo_profesor_cedula=p.cedula where estudiante_has_grupo.estudiante_usuario_idusuario = ?'
     const cursos =await pool.query(sql,req.user.idusuario)
     const sql_pensum = 'select e.nombre,e.cedula,e.pensum_idpensum,p.nombre as pensum  from pensum p inner join estudiante e on p.idpensum=e.pensum_idpensum where e.usuario_idusuario = ?'
@@ -11,11 +12,18 @@ router.get('/cursos', async (req, res) => {
     res.render("links/estudiante/cursosEst",{cursos,pensum})
 })
 
-router.get('/indexest', (req, res) => {
+router.get('/indexest', isLoggedIn, (req, res) => {
+    console.log(req.user)
     res.render("links/estudiante/indexest")
+    
 })
 
-router.get('/indexprofe', (req, res) => {
+router.get('/indexadmin', isLoggedIn, (req, res) => {
+    res.render("links/admin/indexAdmin.hbs")
+    
+})
+
+router.get('/indexprofe', isLoggedIn,(req, res) => {
     res.render("links/profesor/indexProf")
 })
 
