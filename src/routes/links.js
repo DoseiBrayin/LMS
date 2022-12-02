18f9aa42c4b0b66c5c5aed4df17ba4d822e84cfc
DoseiBrayin <ardilabrayan68@gmail.com>
2022-12-02 12:30:30 -0500

    (agrega) se agrega la funcion descargar pdf del estudiante

--- src/routes/links.js
+++ src/routes/links.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const pool = require('../database')
+const PDF = require('pdfkit-construct')
 const router = express.Router()
 const {isLoggedIn,isStudent,isTeacher,isAdmin} = require('../lib/authentication')
+
 
 //RUTAS DEL ESTUDIANTE
 router.get('/cursos',isLoggedIn,isStudent, async (req, res) => {
@@ -31,7 +33,6 @@ router.get('/profe/cursos', isLoggedIn,isTeacher,async(req, res) => {
     const cedula = await pool.query(sql,req.user.idusuario)
     const sql2 = 'SELECT idgrupo,profesor_cedula,dia,hora_inicio,hora_fin,nombre FROM `grupo` INNER JOIN `tiempo` on tiempo_idtiempo=idtiempo INNER JOIN `asignatura` on asignatura_id_asig=id_asig WHERE profesor_cedula = ?;'
     const cursos = await pool.query(sql2,cedula[0].cedula)
-    console.log(cursos)
     res.render("links/profesor/cursoProf",{cursos})
 })
 router.get('/profe/lista:idgrupo', isLoggedIn,isTeacher,async(req, res) => {
@@ -44,6 +45,61 @@ router.get('/profe/descargarlista:idgrupo', isLoggedIn,isTeacher,async(req, res)
     const grupo = req.params
     const sql = 'SELECT e.* FROM `estudiante_has_grupo` INNER JOIN `estudiante` e on estudiante_has_grupo.estudiante_cedula= e.cedula WHERE estudiante_has_grupo.grupo_idgrupo = '+ grupo.idgrupo
     const lista = await pool.query(sql)
-    res.redirect('/links/profe/lista'+grupo.idgrupo)
+
+    //SE CREA EL PDF
+    let doc = new PDF({ margin: 30, size: 'A4'})
+    const stream = res.writeHead(200,{
+        'Content-Type' : 'aplication/pdf',
+        'Content-disposition' : 'attachment;filename="lista.pdf"'
+    })
+
+    const listado = lista.map((data)=>{
+        const estudiantes ={
+            cedula:data.cedula,
+            nombre:data.nombre,
+            email:data.correo,
+            pensum:data.pensum_idpensum,
+            usuario:data.usuario_idusuario,
+        }
+        return estudiantes
+    })
+    console.log(listado)
+    //CREO EL DOCUMENTO
+    doc.addTable([
+        {key:'cedula',label:'cedula',aling:'left'},
+        {key:'nombre',label:'nombre',aling:'left'},
+        {key:'email',label:'email',aling:'left'},
+        {key:'pensum',label:'pensum',aling:'left'},
+        {key:'usuario',label:'user',aling:'left'},
+    ],listado,{
+        headBackground  : '#28a745',
+        headColor: "#FFFFFF",
+        border: null,
+        width: "auto",
+        striped: true,
+        stripedColors: ["#f6f6f6", "#f6f6f6"],
+        cellsPadding: 10,
+        marginLeft: 45,
+        marginRight: 45,
+        headAlign: 'center'
+    })
+
+    doc.setDocumentHeader({}, () => {
+        doc.lineJoin('miter')
+            .rect(0, 0, doc.page.width, doc.header.options.heightNumber).fill("#28a745");
+
+        doc.fill("#FFFFFF")
+            .fontSize(20)
+            .text(`LISTADO DE ESTUDIANTES DEL GRUPO ${grupo.idgrupo}`, doc.header.x, doc.header.y);
+    });
+    
+    doc.on('data',(data)=>{
+        stream.write(data)
+    })
+    doc.on('end',()=>{stream.end()})
+    doc.render()
+    doc.end()
+
+
 })
 module.exports = router
